c15ce682d3de591c3b3644ca6be8bf6cc23d2d93
AfroGrit <jlkitunka@gmail.com>
2024-10-31 11:19:55 +0200

    Add function to load and index data using Minsearch. Reads data from a parquet file, processes responder names, and reformats date column to . Converts data into a dictionary format and indexes it with Minsearch for optimized text and keyword-based search.

---
+++ llm/pmg-assist/pmg-assist/ingest.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+import minsearch
+
+
+DATA_PATH = os.getenv("DATA_PATH", "../data/_pmg_sample_clean.parquet.brotli")
+
+def load_index(data_path=DATA_PATH):
+    df = pd.read_parquet(data_path)
+    df['responder'] = df['responder'].str.replace('to ask the ', '', regex=False)
+    # Convert 'date' column to the desired string format 'YYYY-MM-DD'
+    df['date'] = df['date'].dt.strftime('%Y-%m-%d')
+    # Confirm that the 'date' column is now of type 'object'
+    df['date'] = df['date'].astype('object')
+
+    documents = df.to_dict(orient="records")
+
+    index = minsearch.Index(
+        text_fields=['date', 'id', 'mp', 'responder', 'question', 'answer'],
+        keyword_fields=["id"],
+    )
+
+    index.fit(documents)
+    return index
